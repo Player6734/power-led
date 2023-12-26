6c967c2fe48bb8c449bac5f95cf54c96146ff07a
Player6734 <88460695+Player6734@users.noreply.github.com>
2023-12-26 20:24:36 +0100

    fixed miss-call of ectool

--- power-led-on.sh
+++ power-led-on.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 level=$(cat /sys/class/power_supply/BAT1/capacity)
 if [[ $level -gt 70 ]]; then
-    /usr/bin/local/ectool led power green
+    /usr/local/bin/ectool led power green
 elif [[ $level -gt 45 ]]; then
-    /usr/bin/local/ectool led power white
+    /usr/local/bin/ectool led power white
 elif [[ $level -gt 20 ]]; then
-    /usr/bin/local/ectool led power yellow
+    /usr/local/bin/ectool led power yellow
 else
-    /usr/bin/local/ectool led power red
+    /usr/local/bin/ectool led power red
 fi
